f2385bc6ec54c5d84e10f0b0717d72f7b3758711
Kushagra Arora <kushagra15049@iiitd.ac.in>
2016-10-01 13:04:55 +0530

    movie and song

---
+++ Media.java
@@ -0,0 +1,33 @@
+public abstract class Media implements Comparable, Serializable
+{
+
+}
+
+class Movie extends Media
+{
+	private String director;
+	private String producer;
+	private String certification;
+	String getDirector() { return this.director; }
+	String getProducer() { return this.producer; }
+	String getCertification() {return this.certification; }
+	String toString()
+	{
+		String res = this.title;
+		res += super.toString();
+		res = res +  this.director + "," + this.producer + "," + this.certification;
+		return res;
+	}
+}
+
+class Song extends Media
+{
+	private String movieName;
+	String getMovieName() { return this.movieName; }
+	String toString()
+	{
+		String res = this.title;
+		res += this.movieName;
+		res += super.toString();
+	}
+}
---
+++ check_comparable.java
@@ -0,0 +1,9 @@
+public class check_comparable
+{
+	public static void main(String args[])
+	{
+		String a = "abcd";
+		String b = "abcd";
+		System.out.println(a.compareTo(b));	
+	}
+}
